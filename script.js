7c14ced9ce1d81aaa4c89587f2e16fac2e69383a
Austin Hunt <austin.hunt.012@gmail.com>
2022-12-06 09:06:31 -0600

    dice roll finished

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ for (const field of formFields) {
     `;
     });
 }
+
+function rollDice() {
+    // Calculate the character's attributes based on random numbers between 6 and 18
+    let strength = Math.floor(Math.random() * 13) + 6;
+    let dexterity = Math.floor(Math.random() * 13) + 6;
+    let constitution = Math.floor(Math.random() * 13) + 6;
+    let intelligence = Math.floor(Math.random() * 13) + 6;
+    let wisdom = Math.floor(Math.random() * 13) + 6;
+    let charisma = Math.floor(Math.random() * 13) + 6;
+
+
+    // Display the character's attributes
+    let attributesDiv = document.getElementById("attributes");
+    attributesDiv.innerHTML = "Strength: " + strength + "<br>" +
+        "Dexterity: " + dexterity + "<br>" +
+        "Constitution: " + constitution + "<br>" +
+        "Intelligence: " + intelligence + "<br>" +
+        "Wisdom: " + wisdom + "<br>" +
+        "Charisma: " + charisma;
+}
